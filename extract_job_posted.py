7fffa19be4a6ca47368db942b7d3f1689f0fc7ad
Raymond Owhondah <raymond.owhondah@nhschocies.net>
2023-06-21 09:00:14 +0100

    transformation stage

--- extract_job_posted.py
+++ extract_job_posted.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 import json
+from datetime import datetime
+import os
 
 
 
@@ -26,4 +28,20 @@ def get_job_posted(headers):
 
 
         
+def transformed_raw_data():
+    csv_data = []
+    with open('raw/raw_job_posted_data.json', 'r') as job_file:
+        reader = json.load(job_file)
+        data = reader['data']
         
+    
+    # reading the data to pandas dataframe, first we create a column name
+    column_names = ['employer_website', 'job_id', 'job_employment_type', 'job_title', 'job_apply_link', 'job_description', 'job_city', 'job_country', 'job_posted_at_timestamp','employer_company_type']
+    job_df = pd.DataFrame(data, columns=column_names)
+    
+    #converting the timestamp vale to date
+    print(job_df['job_posted_at_timestamp'])
+
+    
+    # filepath = os.path('API_ETL_PIPELINE/Transfromed/job_posted_data.csv')   
+    # job_df.to_csv(filepath)
